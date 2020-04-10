50fee9e1505265cdb11045e94fc6282e83dc2b46
greenywd <greenydev@greenmail.net.au>
2020-04-10 20:15:33 +0930

    Add metavars to args and add version to check in downloadPaper

--- updater.py
+++ updater.py
@@ -23,11 +23,11 @@ class Paper:
         builds = self.getBuildsForVersion(version)
         latest_build = builds['latest']
 
-        if (os.path.isfile('builds/paper-%s.jar' % (str(latest_build)))):
-            raise FileExistsError("Build '%s' already exists in /builds." % (build))
+        if (os.path.isfile('builds/%s/paper-%s.jar' % (version, str(latest_build)))):
+            raise FileExistsError("Build '%s' already exists in /builds/%s/." % (build, version))
 
         # If build doesn't exist in /builds, download it.
-        print('Build does not exist in builds/%s/, downloading now.' % (version))
+        print("Build %s does not exist in builds/%s/, downloading now." % (latest_build, version))
         url = "https://papermc.io/api/v1/paper/%s/%s/download" % (version, build)
         r = requests.get(url, allow_redirects=True)
         filepath = 'builds/%s/' % (version)
@@ -67,26 +67,32 @@ class Paper:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='paper-updater', description='Paper Minecraft Server Helper', epilog='If no arguments are given, the latest version of Paper will automatically be downloaded.')
-    parser.add_argument('-d', '--server-dir', type=str, help='Full directory of the Paper Server to be updated')
+    parser.add_argument('-d', '--server-dir', type=str, help='Full directory of the Paper Server to be updated', metavar='/home/minecraft/servers/')
     parser.add_argument('-r', '--recursive', action='store_true', help='Update paper in every directory located inside of -d/--server-dir')
     parser.add_argument('-v', '--versions', action='store_true', help='List versions of the Paper Minecraft Server')
-    parser.add_argument('-b', '--builds', type=str, help='List builds of a specfic version of the Paper Minecraft Server')
-    parser.add_argument('-o', '--output-file', action='store_true', help='Filename that will be given to the server jar. Default is paper.jar.')
+    parser.add_argument('-b', '--builds', type=str, help='List builds of a specfic version of the Paper Minecraft Server', metavar='')
+    parser.add_argument('-o', '--output-file', type=str, help='Filename that will be given to the server jar. Default is paper.jar.', metavar='paper.jar')
+    parser.add_argument('--download-only', type=str, help='Download the latest build of Paper of the specified Minecraft version.', metavar='version')
     args = parser.parse_args()
 
     paper = Paper()
-    print (args.builds)
+
+    # ---------------- List versions of Paper ---------------- #
     if args.versions:
         print('\n'.join(paper.getVersions()))
         quit()
 
+    # ---------------- List builds of a specific version of Paper ---------------- #
     if args.builds:
         print('\n'.join(paper.getBuildsForVersion(args.builds)['all']))
         quit()
 
+    # ---------------- Download latest build of Paper ---------------- #
+    if args.download_only:
+        paper.downloadPaper(version=args.download_only)
+
     # ---------------- Updating Paper ---------------- #
     if args.server_dir:
         server_dir = args.server_dir
-
         if args.recursive:
             paper.copyLatestPaperRecursively(server_dir)
